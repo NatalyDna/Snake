db4979527199c9d53b7f234a57b9dc1a7cd2276d
NatalyDna <n9184360296@yandex.ru>
2017-03-16 16:28:00 +0400

    Создание конструктов.Инкапсуляция.

--- ConsoleApp1/ConsoleApp1/Point.cs
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -12,6 +12,14 @@ namespace ConsoleApp1
         public int y;
         public char sym;
 
+        public Point() {// создаем конструктор 
+           }
+        public Point(int _x, int _y, char _sym) {
+            x =_x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw () {
 
             Console.SetCursorPosition(x, y);// выводим координаты точек на экран
